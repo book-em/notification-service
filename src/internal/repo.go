d81242e735ad5a76df836a8adaf450493604f7ea
Hristina Adamovic <hristinacina@gmail.com>
2025-10-24 18:23:35 +0200

    feat: Add get user notifications API

--- src/internal/repo.go
+++ src/internal/repo.go
@@ -6,11 +6,12 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Repository interface {
 	Save(ctx context.Context, notification *Notification) (*Notification, error)
-	FindByReceiverID(ctx context.Context, receiverID string) ([]Notification, error)
+	FindByReceiverID(ctx context.Context, receiverID uint, limit int) ([]Notification, error)
 	MarkAsRead(ctx context.Context, id string) error
 }
 
@@ -34,10 +35,14 @@ func (r *repository) Save(ctx context.Context, n *Notification) (*Notification, 
 	return n, nil
 }
 
-func (r *repository) FindByReceiverID(ctx context.Context, receiverID string) ([]Notification, error) {
+func (r *repository) FindByReceiverID(ctx context.Context, receiverID uint, limit int) ([]Notification, error) {
 	coll := r.db.Collection("notifications")
 
-	cursor, err := coll.Find(ctx, bson.M{"receiverId": receiverID})
+	opts := options.Find().
+		SetSort(bson.D{{Key: "createdAt", Value: -1}}).
+		SetLimit(int64(limit))
+
+	cursor, err := coll.Find(ctx, bson.M{"receiverId": receiverID}, opts)
 	if err != nil {
 		return nil, err
 	}
