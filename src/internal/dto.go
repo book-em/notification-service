a140f011cf1bea06fc7e41e0f52ac76c1a597b97
Hristina Adamovic <hristinacina@gmail.com>
2025-10-24 15:50:15 +0200

    feat: Add notification models and dtos

--- src/internal/dto.go
+++ src/internal/dto.go
@@ -1,5 +1,27 @@
 package internal
 
+import "time"
+
+type NewNotificationDTO struct {
+	ReceiverID  string           `json:"receiverId"`
+	Type        NotificationType `json:"type"`
+	Subject     string           `json:"subject"`
+	Object      string           `json:"object"`
+	StarsNumber int              `json:"starsNumber,omitempty"`
+}
+
 type NotificationDTO struct {
-	ID uint `json:"id"`
+	ID          string           `json:"id"`
+	ReceiverID  string           `json:"receiverId"`
+	Type        NotificationType `json:"type"`
+	Subject     string           `json:"subject"`
+	Object      string           `json:"object"`
+	StarsNumber int              `json:"starsNumber,omitempty"`
+	IsRead      bool             `json:"isRead"`
+	CreatedAt   time.Time        `json:"createdAt"`
+}
+
+type NotificationPreferencesDTO struct {
+	UserID       string                    `json:"userId"`
+	EnabledTypes map[NotificationType]bool `json:"enabledTypes"`
 }
--- src/internal/model.go
+++ src/internal/model.go
@@ -1,9 +1,35 @@
 package internal
 
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type NotificationType string
+
+const (
+	ReservationRequested NotificationType = "reservation_requested"
+	ReservationCancelled NotificationType = "reservation_cancelled"
+	HostReviewed         NotificationType = "host_reviewed"
+	RoomReviewed         NotificationType = "room_reviewed"
+	ReservationAccepted  NotificationType = "reservation_accepted"
+	ReservationDeclined  NotificationType = "reservation_declined"
+)
+
 type Notification struct {
-	ID uint
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	ReceiverID  string             `bson:"receiverId" json:"receiverId"`
+	Type        NotificationType   `bson:"type" json:"type"`
+	Subject     string             `bson:"subject" json:"subject"`
+	Object      string             `bson:"object,omitempty" json:"object"`
+	StarsNumber int                `bson:"starsNumber,omitempty" json:"starsNumber,omitempty"`
+	IsRead      bool               `bson:"isRead" json:"isRead"`
+	CreatedAt   time.Time          `bson:"createdAt" json:"createdAt"`
 }
 
-type Preference struct {
-	ID uint
+type NotificationPreferences struct {
+	ID           primitive.ObjectID        `bson:"_id,omitempty" json:"id"`
+	UserID       string                    `bson:"userId" json:"userId"`
+	EnabledTypes map[NotificationType]bool `bson:"types" json:"types"`
 }
